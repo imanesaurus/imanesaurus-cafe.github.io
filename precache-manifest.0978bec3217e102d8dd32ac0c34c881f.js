313319833839ffd34b0b31ad79971cf16e5fc0eb
imanesaurus <pirmansah.otoluwa@gmail.com>
2021-02-12 08:51:17 +0700

    Initial commit

---
+++ precache-manifest.0978bec3217e102d8dd32ac0c34c881f.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a705f14690a80fa39ec",
+    "url": "/static/js/app.d57af45b.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "1424c1a50bebd4b5ecc5",
+    "url": "/static/js/2.b97547c9.chunk.js"
+  },
+  {
+    "revision": "794588dfbef6e9a7669ccf82cd3b6db9",
+    "url": "/static/media/cafe.44c7420f.png"
+  },
+  {
+    "revision": "7d51f2275697755190416561c2b547c0",
+    "url": "/static/media/donut.da724eee.png"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "2d051e27c41b4d906962ff93874db956",
+    "url": "/static/media/Logo.f712a129.jpg"
+  },
+  {
+    "revision": "795ecb7cb88c68b3be170b26d92da244",
+    "url": "/static/media/Logo.d6a4d326.png"
+  },
+  {
+    "revision": "4f1cb2cac2370cd5050681232e8575a8",
+    "url": "/static/media/favicon.fa4783d1.png"
+  },
+  {
+    "revision": "e4f372b664360f0e68f02c08bd2d75dc",
+    "url": "/static/media/fullset.1e4b2a2f.jpg"
+  },
+  {
+    "revision": "ef1224697cefa83e8b1b774e8f51b95c",
+    "url": "/static/media/paket_1.cb8934de.jpg"
+  },
+  {
+    "revision": "1fd218cf66536c27318c8ce3ed8127f7",
+    "url": "/static/media/paket_2.34233474.png"
+  },
+  {
+    "revision": "c37c2dbbf2c10007ac37bc690900d062",
+    "url": "/static/media/paket_3.1b57e24a.jpeg"
+  },
+  {
+    "revision": "e683fd3db8b29f26c2b7da5d2f50829c",
+    "url": "/static/media/screens.a2c3754e.png"
+  },
+  {
+    "revision": "b8895f17edbefb9f51b1f863c9811e44",
+    "url": "/static/media/splash.8463f3d1.png"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "6d622ad666c64a4ba1188860ca99b1ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "e4bd7bae898eea74023bf2e1ab19cd6f",
+    "url": "/static/js/2.b97547c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "50b1bd72d8b0c569ffcad83d622ec176",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "eadac3b481cba8a842b7c9f21a773ac9",
+    "url": "/manifest.json"
+  }
+];
